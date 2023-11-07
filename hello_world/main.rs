7cdce3acd2a350dce434f1c5440a22090679abe1
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-11-06 23:03:20 -0600

    first example

---
+++ hello_world/main.rs
@@ -0,0 +1,10 @@
+// My first program compiled with rustc
+
+fn main() {
+    println!("Hola 🦀!") // this is a Macro, not a function: "!" is used in macros
+}
+
+// Compile with:
+// $ rustc main.rs
+// Run with:
+// $ ./main
