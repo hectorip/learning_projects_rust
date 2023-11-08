51fab053e5433cc124b3b3230491878b010752eb
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-11-07 22:51:09 -0600

    WIP

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -4,6 +4,7 @@ fn main() {
     println!("Hola 🦀!") // this is a Macro, not a function: "!" is used in macros
 }
 
+// Compilation and execution are two separate steps
 // Compile with:
 // $ rustc main.rs
 // Run with:
