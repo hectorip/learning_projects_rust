d7e68a331205f103fe6fdd6e362a52d64fff6831
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-12-03 23:23:36 -0600

    WIP

--- timers/src/main.rs
+++ timers/src/main.rs
@@ -1,4 +1,7 @@
 // This program is a simple timer that counts down from a specified number of seconds.
+use std::thread;
+use std::time::Duration;
+
 
 fn main() {
     println!("------");
@@ -6,23 +9,19 @@ fn main() {
     println!("Bienvenido al temporizador");
 
     // Wait 10 seconds
-    while true {
+    loop {
         println!("¿Cuántos segundos *quieres* esperar?");
         let mut input = String::new();
         std::io::stdin()
             .read_line(&mut input)
-            .expect("No se pudo leer la línea 💀");
+            .expect("No se pudo leer la línea");
 
         let seconds: u64 = input.trim().parse().expect("Error al convertir el número: ");
         if seconds > 0 {
             println!("Esperando {} segundos...", seconds);
-            std::thread::sleep(std::time::Duration::from_secs(seconds));
+            thread::sleep(Duration::from_secs(seconds));
             println!("¡Han pasado {} segundos!", seconds);
             // break;
         }
-        println!("El número debe ser mayor a 0");
     }
 }
-
-// Shingo Nakamura - Hakodate (SineRider Remix)
-// https://www.youtube.com/watch?v=ZQ7r3mZzXO0
