c7fd5563c697ef790c6588b76d8a0910dddca7e4
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-12-06 00:00:11 -0600

    WIP

--- regexp_evaluator/src/main.rs
+++ regexp_evaluator/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Evaluating regular expressions");
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read line");
+
+    // Create a vector of chars from the input string
+    let mut chars: Vec<char> = input.trim().chars().collect();
+    println!("chars: {:?}", chars);
+
+    // Create a vector of tokens from the vector of chars
+    let mut tokens: Vec<String> = Vec::new();
+    let mut token: String = String::new();
+    // Do not know what this does
+    for c in chars {
+        if c == '(' || c == ')' || c == '|' || c == '*' {
+            if token.len() > 0 {
+                tokens.push(token);
+                token = String::new();
+            }
+            tokens.push(c.to_string());
+        } else {
+            token.push(c);
+        }
+    }
 }
