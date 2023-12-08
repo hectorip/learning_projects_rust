5d9a01f6e352df35529ec0ba0473632b09200793
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-12-07 23:45:45 -0600

    Program

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,3 +1,8 @@
+// This will be a simple interpreter that will take in a string and print it out.
 fn main() {
-    println!("Hello, world!");
+    // read program from file in the first argument
+    let program = std::env::args().nth(1).expect("Please provide a program");
+
+    println!("Running the interpreter");
+    println!("Program: {:?}", program);
 }
