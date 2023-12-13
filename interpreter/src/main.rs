39e6f4721e4b385cf8fecbf8b4d976bc4d895326
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-12-12 23:58:00 -0600

    New projects

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,4 +1,5 @@
 // This will be a simple interpreter that will take in a string and print it out.
+
 fn main() {
     // read program from file in the first argument
     let program = std::env::args().nth(1).expect("Please provide a program");
@@ -6,3 +7,5 @@ fn main() {
     println!("Running the interpreter");
     println!("Program: {:?}", program);
 }
+
+// The program will be a list of instructions, each instruction will be a tuple
