c853684af1c5c1f7442349bfc6a56abfb9bd694b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-12-18 23:35:45 -0600

    WIP

--- concurrent_test/src/main.rs
+++ concurrent_test/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
+    // Probando Async/Await
+
+    let mut input = String::new();
+
 }
--- cpu_simulation/src/main.rs
+++ cpu_simulation/src/main.rs
@@ -37,7 +37,5 @@ struct CPU {
 fn cpu(programa: Vec<Instruction>) {
     // Inicializa el CPU y la RAM
     registros = {"Ra": 0, "Rb": 0, "Rc": 0, "Rd": 0, "Re": 0, "Rf": 0, "Rg": 0, "Rh": 0}
-
     // Imprime el estado de la CPU
-
 }
