e6a46c3d6d8bd6d6a2eaa3e96a3973ec7f65b27d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-01-05 23:54:08 -0600

    Weeks difference working

--- how_many_weeks/src/main.rs
+++ how_many_weeks/src/main.rs
@@ -12,15 +12,15 @@ fn main() {
 fn calculate_weeks(date: &str) -> u32 {
     let mut weeks = 0;
     // Get current date
-    let current_date = Utc::today();
+    let current_date = Utc::now().date_naive();
     let from_date = NaiveDate::parse_from_str(date, "%d/%m/%Y").unwrap();
     std::println!("Current date: {}", current_date);
     std::println!("From date: {}", from_date);
     // Get the difference between dates
 
-    let difference = current_date.signed_duration_since(&from_date as &Date<Tz>);
-    std::println!("Difference: {}", difference);
+    let difference = current_date.signed_duration_since(from_date);
+    std::println!("Difference: {}", difference.num_weeks());
     // difference.num_weeks() as u32
 
-    return 1;
+    return difference.num_weeks() as u32;
 }
