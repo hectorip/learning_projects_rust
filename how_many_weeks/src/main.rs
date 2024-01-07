fdeab920cfac5192614f7ea75ddfec1bd156e818
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-01-06 23:21:41 -0600

    Finish refactor

--- how_many_weeks/src/main.rs
+++ how_many_weeks/src/main.rs
@@ -5,21 +5,22 @@
 use chrono::prelude::*;
 
 fn main() {
-    let input = std::env::args().nth(1).expect("Por favor, ingrese una fecha en formato dd/mm/aaaa");
-    calculate_weeks(&input);
+    let to_date_input = std::env::args().nth(1).expect("Por favor, ingrese una fecha en formato dd/mm/aaaa");
+    let from_date_input: Option<String> = std::env::args().nth(2);
+    
+    let to_date = NaiveDate::parse_from_str(&to_date_input, "%d/%m/%Y").unwrap();
+    let from_date = match from_date_input {
+        Some(date) => NaiveDate::parse_from_str(&date, "%d/%m/%Y").unwrap(),
+        None => Utc::now().date_naive(),
+    };
+    calculate_weeks(&from_date, &to_date);
+
 }
 
-fn calculate_weeks(from_date: NaiveDate, to_date: NaiveDate) -> u32 {
-    // Get current date
-    let current_date = Utc::now().date_naive();
-    let from_date = NaiveDate::parse_from_str(date, "%d/%m/%Y").unwrap();
-    std::println!("Current date: {}", current_date);
-    std::println!("From date: {}", from_date);
-    // Get the difference between dates
+fn calculate_weeks(from_date: &NaiveDate, to_date: &NaiveDate) -> u32 {
 
-    let difference = current_date.signed_duration_since(from_date);
+    let difference = from_date.signed_duration_since(*to_date);
     std::println!("Difference: {}", difference.num_weeks());
-    // difference.num_weeks() as u32
 
     return difference.num_weeks() as u32;
 }
