7e81916bf613868cf303c1a98af0cd6a37c7448b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-01-18 16:36:57 -0600

    Clonado de estructuras

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,25 +1,43 @@
+// Struct -> Son como objetos con campos y métodos
+
 
 #[derive(Debug)]
 struct Point{
     x: i32,
-    y: i32
+    y: i32,
+    name: String,
 }
 
 impl Point {
-
     fn distance_from_zero(&self) -> i32 {
         (&self.x.pow(2) + &self.y.pow(2)).pow(1/2)
     }
 }
 
-
 fn main() {
-    let point = Point{
+    let mut point = Point{
         x:1,
         y:1,
+        name: String::from("Unit"),
     };
     println!("X: {}", point.x);
     println!("Y: {}", point.y);
+    println!("Nombre del punto: {}", point.name);
     println!("Mi punto {point:?}");
     println!("Distancia desde el origen: {}", point.distance_from_zero());
+    
+    // Si algún campo del struct es dinámico cuando se clona o hace update, se vuelve inválido
+
+    let point_2 = Point{
+       x: 2,
+       // ..point // Esto lo haría inválido
+       y: 2,
+       name: String::from(&point.name[..]) // Pero sí podemos copiar la cadena
+    };
+
+    point.name = String::from("Otro nombre");
+    println!("Intentando usar el punto original: {:?}", point);
+    println!("Punto nuevo: {:?}", point_2);
+
+
 }
