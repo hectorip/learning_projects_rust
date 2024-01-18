4d507be8a4e09f8cc735f14676f400f17567e4e2
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-01-17 23:00:33 -0600

    loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,30 @@
+
 fn main() {
-    println!("Hello, world!");
+    // Explorando los tres diferentes tipos de ciclos que tiene Rust
+    //
+    // Ciclo infinito
+    //
+    let mut i = 0;
+    loop {
+        i += 1;
+        println!("Loop: Voy a correr mientras el programa no se detenga o se llame a 'break'");
+        println!("Ciclo: {i}");
+        if i == 10 {
+            break;
+        }
+    }
+
+    let sep = "*".repeat(50);
+    println!("{}", sep);
+    while i < 20 {
+        println!("While: voy a correr mientras una condición se cumpla");
+        println!("Ciclo: {i}");
+        i += 1;
+    }
+    println!("{}", sep);
+    for x in 1..=10{
+        println!("For: voy a recorrer una colección hasta terminarla.");
+        println!("Elemento actual: {x}");
+    
+    }
 }
