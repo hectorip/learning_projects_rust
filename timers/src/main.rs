ae8b9f295355ea75584a269c53a8335cc9d4413f
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-01-20 23:23:22 -0600

    Working on LRU

--- timers/src/main.rs
+++ timers/src/main.rs
@@ -2,27 +2,27 @@
 use std::thread;
 use std::time::Duration;
 
-
 fn main() {
     println!("------");
-    println!("\n\n\n");
-    println!("Bienvenido al temporizador");
+    println!("Bienvenido al temporizador ⏰ de Rust 🦀");
 
-    // Wait 10 seconds
+    // Wait X Seconds
     loop {
-        println!("¿Cuántos segundos *quieres* esperar?");
+        println!("¿Cuántos segundos *quieres* esperar? - Si quieres salir presiona Ctrl+C");
         let mut input = String::new();
         std::io::stdin()
             .read_line(&mut input)
             .expect("No se pudo leer la línea");
 
-        let seconds: u64 = input.trim().parse().expect("Error al convertir el número: ");
+        let seconds: u64 = input
+            .trim()
+            .parse()
+            .expect("Error al convertir el número: ");
+
         if seconds > 0 {
             println!("Esperando {} segundos...", seconds);
             thread::sleep(Duration::from_secs(seconds));
             println!("¡Han pasado {} segundos!", seconds);
-            // break;
         }
     }
 }
-    // 💀
