0bb224508bb82ffaba4099c57269a02f6837032c
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-01-21 23:58:55 -0600

    LRU avanzando pero con problemas

--- lru_cache/src/main.rs
+++ lru_cache/src/main.rs
@@ -7,15 +7,23 @@
 // int get(int key) Return the value of the key if the key exists, otherwise return -1.
 // void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
 // The functions get and put must each run in O(1) average time complexity.
-
+use std::collections::HashMap;
+use std::collections::VecDeque;
 
 fn main() {
     println!("LRU Cache Implementation");
+    let mut cache = LRUCache::new(2);
+    cache.put(1, 5);
+    cache.put(2, 10);
+    cache.put(3, 15);
+    println!("Cache: {:?}", cache);
 }
+
+#[derive(Debug)]
 struct LRUCache {
-    capacity: usize,
+    capacity: usize, // The number of elements the cache can hold
     cache: HashMap<i32, i32>,
-    queue: Vec<i32>
+    queue: VecDeque<i32>, // We need to remove elemenents from both sides
 }
 
 impl LRUCache {
@@ -23,34 +31,36 @@ impl LRUCache {
         LRUCache {
             capacity,
             cache: HashMap::new(),
-            queue: Vec::new()
+            queue: VecDeque::with_capacity(capacity),
         }
     }
 
-    fn get(&mut self, key: i32) -> i32 {
-        match self.cache.get(&key) {
+    fn get(&mut self, key: i32) -> Option<i32> {
+        let result = self.cache.get(&key);
+        match result {
             Some(value) => {
-                self.queue.retain(|&x| x != key);
-                self.queue.push(key);
-                *value
-            },
-            None => -1
+                self._update_queue(key);
+                Option::from(*value)
+            }
+            None => Option::None,
         }
     }
 
+    fn _update_queue(&mut self, key: i32) {
+        self.queue.retain(|&x| x != key); // Remove the key from the queue
+        self.queue.insert(self.capacity - 1, key);
+    }
     fn put(&mut self, key: i32, value: i32) {
         if self.cache.contains_key(&key) {
             self.cache.insert(key, value);
-            self.queue.retain(|&x| x != key);
-            self.queue.push(key);
+            self._update_queue(key);
         } else {
             if self.cache.len() == self.capacity {
-                let key_to_remove = self.queue.remove(0);
+                let key_to_remove: i32 = self.queue.remove(0).expect("Error fatal");
                 self.cache.remove(&key_to_remove);
             }
             self.cache.insert(key, value);
-            self.queue.push(key);
+            self.queue.insert(self.capacity - 1, key);
         }
     }
 }
-
