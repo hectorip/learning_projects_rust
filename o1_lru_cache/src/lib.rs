45cde6a3851a3c36141c6819b3cd603706df2c30
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-02-19 23:58:50 -0600

    WIP LRU

--- o1_lru_cache/src/lib.rs
+++ o1_lru_cache/src/lib.rs
@@ -5,13 +5,13 @@
 // Una lista doblemente ligada cumple con ese objetivo
 //
 //
-use std::rc::Rc; // Necesitamos Rc para poder tener referencias a los nodos
+use std::{cell::RefCell, rc::Rc}; // Necesitamos Rc para poder tener referencias a los nodos
 
 struct Node {
     value: i32,
     key: i32,
-    prev: Option<Rc<Self>>,
-    next: Option<Rc<Self>>,
+    prev: Option<Rc<RefCell<Self>>>,
+    next: Option<Rc<RefCell<Self>>>,
 }
 
 impl Node {
@@ -26,8 +26,8 @@ impl Node {
 }
 
 struct DoubleLinkedList {
-    head: Option<Rc<Node>>,
-    tail: Option<Rc<Node>>,
+    head: Option<Rc<RefCell<Node>>>,
+    tail: Option<Rc<RefCell<Node>>>,
 }
 
 impl DoubleLinkedList {
@@ -37,32 +37,45 @@ impl DoubleLinkedList {
             tail: None,
         }
     }
-    fn push_front(&mut self, key: i32, value: i32) -> Rc<Node> {
-        let mut node = Rc::new(Node::new(key, value));
-        match self.head {
-            Some(_) => {
-                node.next = Some(*self.head.as_ref().unwrap());
-                self.head.as_mut().unwrap().prev = Some(node);
-            }
-            None => {
-                self.tail = Some(node);
-            }
+    fn push_front(&mut self, key: i32, value: i32) -> Rc<RefCell<Node>> {
+        let node = Rc::new(RefCell::new(Node::new(key, value)));
+        if let Some(head) = self.head.take() {
+            head.borrow_mut().prev = Some(Rc::clone(&node));
+            node.borrow_mut().next = Some(head);
+            self.head = Some(Rc::clone(&node));
+        } else {
+            self.head = Some(Rc::clone(&node));
+            self.tail = Some(Rc::clone(&node)); // Maybe we can avoid the clone
         }
-        self.head = Some(node);
         node
     }
-    fn move_front(&mut self, node: &mut Rc<Node>) {
-        // Mueve un nodo EXISTENTE al frente de la lista
-        if let Some(mut prev) = node.prev {
-            prev.next = node.next;
+    fn move_front(&mut self, node: Rc<RefCell<Node>>) {
+        // Recibe una referencia un nodo y lo mueve al frente de la lista
+
+        // Si el nodo ya está al frente no hacemos nada
+        if Rc::ptr_eq(&self.head.as_ref().unwrap(), &node) {
+            return;
         }
-        node.next = self.head;
-        self.head = Some(*node);
+
+        if let Some(prev) = node.borrow_mut().prev.take() {
+            prev.borrow_mut().next = node.borrow_mut().next.take(); // No iporta si es None
+                                                                    // Si el nodo es la cola, tenemos apuntar la cola al nodo previo.
+            if Rc::ptr_eq(&self.tail.as_ref().unwrap(), &node) {
+                self.tail = Some(Rc::clone(&prev));
+            }
+            // ?=
+            if Some(next) = node.borrow_mut().next.take() {
+                next.borrow_mut().prev = Some(Rc::clone(&prev));
+            }
+        }
+
+        let next = node.borrow_mut().next.as_ref();
+        // En otro caso, lo ponermos como el nuevo head
     }
-    fn pop_tail(&self) -> Option<Rc<Node>> {
+    fn pop_tail(&mut self) -> Option<Rc<RefCell<Node>>> {
         if let Some(tail) = self.tail {
-            self.tail = tail.prev;
-            self.tail.unwrap().next = None;
+            self.tail = tail.borrow().prev;
+            self.tail.unwrap().borrow_mut().next = None;
             Some(tail)
         } else {
             None
@@ -74,7 +87,7 @@ impl DoubleLinkedList {
 
 pub struct LRUCache {
     capacity: usize,
-    cache: std::collections::HashMap<i32, Rc<Node>>,
+    cache: std::collections::HashMap<i32, Rc<RefCell<Node>>>,
     queue: DoubleLinkedList,
     _current_capacity: usize,
 }
@@ -89,22 +102,22 @@ impl LRUCache {
         }
     }
     pub fn get(&mut self, key: i32) -> i32 {
-        if let Some(node) = self.cache.get_mut(&key) {
-            self.queue.move_front(node);
-            node.value
+        if let Some(node) = self.cache.get(&key) {
+            self.queue.move_front(*node);
+            node.borrow().value
         } else {
             -1
         }
     }
     pub fn put(&mut self, key: i32, value: i32) {
-        if let Some(node) = self.cache.get_mut(&key) {
-            node.value = value;
-            self.queue.move_front(node)
+        if let Some(node) = self.cache.get(&key) {
+            node.borrow().value = value;
+            self.queue.move_front(*node)
         } else {
             if self._current_capacity >= self.capacity {
                 self._current_capacity -= 1;
                 let node = self.queue.pop_tail().unwrap();
-                self.cache.remove(&node.key);
+                self.cache.remove(&node.borrow().key);
             }
             self._current_capacity += 1;
             let node = self.queue.push_front(key, value);
