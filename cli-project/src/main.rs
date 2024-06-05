a0e51d81048658819c376211f0559349893f698f
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-06-04 23:57:50 -0600

    cli project clean

--- cli-project/src/main.rs
+++ cli-project/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     // Cli project proposed by the Rust Book
+    // This is a project to learn the basics of Rust
+    // while applying the learned concpets to a real project
+    // The project is a simple command line tool that takes a string
+    // I hope to learn how to render text in a better way
+    // Question: Does it exist something like BubbleTea for Rust?
 }
